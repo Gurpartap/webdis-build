91b4c84a065cfbc095a567cd46450ffc7888a181
Gurpartap Singh <contact@gurpartap.com>
2014-09-09 20:49:25 +0530

    Route stdout to /dev/null

--- script/clean.sh
+++ script/clean.sh
@@ -2,5 +2,5 @@
 # Usage: ./clean.sh
 
 docker rm -f webdis-build 2> /dev/null
-docker rmi -f gurpartap/webdis-build
-docker rmi -f gurpartap/webdis
+docker rmi -f gurpartap/webdis-build 2> /dev/null
+docker rmi -f gurpartap/webdis 2> /dev/null
